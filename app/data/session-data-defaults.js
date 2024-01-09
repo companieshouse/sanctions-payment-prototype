30e8074dd54be7ea221e07c47976e9f8545c36b3
mtuffs <mtuffs@companieshouse.gov.uk>
2024-01-09 09:51:07 +0000

    creating prototype

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,5 +1,15 @@
 module.exports = {
 
-  // Insert values here
+    // Insert values here
+  
+    "email": [ "user@testcompany.co.uk" ],
+    //remember to put the comma back at the end of line 25!
+    "contact-full-name": [ "Joe Bloggs" ],
+    // Set the "are we signed in?" variable (gSignedIn) to FALSE as its default
+    // "gSignedIn": [ "false" ],
+    // signOutFirstTrigger is specific to the sign-out page to ensure the error messaging can come into play
+    // "signOutFirstTrigger": [ "yes" ],
+
+
 
 }
