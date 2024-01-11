aa2d486f131cfe9cdf23419fab4fe22774febe54
mtuffs <mtuffs@companieshouse.gov.uk>
2024-01-11 14:42:16 +0000

    edited

--- app/routes.js
+++ app/routes.js
@@ -44,3 +44,59 @@ router.post('/v1/filter-question', function(request, response) {
 //         response.redirect("/v1/enter-email-address")
 //     }
 // })
+
+
+// Errors -  v1 code
+module.exports = function (router) {
+    router.post('/v1/filter-question', function (req, res) {
+        var errors = [];
+        var errorApply = false;
+
+        if (req.session.data['apply-to-you'] === '') {
+            errorApply = true;
+            errors.push({
+                text: 'Select any that apply',
+                href: '#applyToYou'
+            });
+        } else {
+            // Required fields have been captured and as such set the "are we signed in?" variable (gSignedIn) to TRUE
+            errorApply = true;  // Remove 'var' from here
+            res.redirect('/v1/sign-in');
+        }
+    });
+};
+
+
+
+// ******* ERRORS - FILTER QUESTION  ************
+router.get('/v1/filter-question', function (req, res) {
+    // Set URl
+    res.render('v1/filter-question', {
+      currentUrl: req.originalUrl
+    })
+  })
+  
+  router.post('/v1/filter-question', function (req, res) {
+    // Create empty array
+    var errors = []
+  
+    // Check if user has filled out a value
+    if (req.session.data['apply-to-you'] == '') {
+      // No value so add error to array
+      errors.push({
+        text: 'Select any that apply',
+        href: '#applyToYou'
+      })
+  
+      // Re-show page with error value as true so errors will show
+      res.render('v1/filter-question', {
+        errorEmail: true, 
+        errorList: errors
+      })
+    } else {
+      // User inputted value so move to next page
+      res.redirect('/v1/sign-in')
+    }
+  })
+
+
