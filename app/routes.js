fad845907808d74342976a4bd8715222382a7879
James Francis <jfranciswebdesign@users.noreply.github.com>
2024-01-12 14:12:51 +0000

    Separated routes

--- app/routes.js
+++ app/routes.js
@@ -7,7 +7,10 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 
-// router.use('/', require('./routes/v1-routes.js'))
+
+
+router.use('/', require('./routes/v1-routes.js'))
+
 
 // Filter question
 // router.post('/v1/filter-question', function(request, response) {
@@ -100,39 +103,5 @@ const router = govukPrototypeKit.requests.setupRouter()
 //     }
 //   })
 
-  // ******* filter-question ********************************
-  router.get('/v1/filter-question-copy', function (req, res) {
-    // Set URl
-    res.render('v1/filter-question-copy', {
-      currentUrl: req.originalUrl
-    })
-  })
-  
-  router.post('/v1/filter-question-copy', function (req, res) {
-    // Create empty array
-    var errors = []
-  
-    // Check if user has filled out a value
-    if (typeof req.session.data['applyToYou'] === 'undefined') {
-      // No value so add error to array (the Error summary)
-      errors.push({
-        text: 'You must select a reason',
-        href: '#applyToYou'
-      })
-  
-      // Re-show page with error value as true so errors will show
-      res.render('v1/filter-question-copy', {
-        errorApplyToYou: true,
-        errorList: errors
-      })
-    } else {
-          // User inputted a specific ,
-          if (req.session.data['applyToYou'] == 'none') {   // this was === which doesn't work, changing to == seems to work?!
-            res.redirect('/v1/sign-in')
-          } else {
-            // User inputted any other value
-            res.redirect('/v1/stop-cannot-use-online-service')
-          }
-    }
-  })
+
 
