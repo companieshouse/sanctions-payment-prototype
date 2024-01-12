a960a5a76c36575570e3e4a1aa5979165cf90629
mtuffs <mtuffs@companieshouse.gov.uk>
2024-01-12 15:03:47 +0000

    routing

--- app/routes/v1-routes.js
+++ app/routes/v1-routes.js
@@ -50,34 +50,29 @@ const router = govukPrototypeKit.requests.setupRouter()
 
 
 
-    // ******* ERROR penalty details ********************************
-    router.post('/v1/penalty-details', function (req, res) {
-      // Create empty array
-      var errors = []
+    // // ******* ERROR penalty details ********************************
+    // router.post('/v1/penalty-details', function (req, res) {
+    //   // Create empty array
+    //   var errors = []
     
-      // Check if user has filled out a value
-      if (typeof req.session.data['applyToYou'] === 'undefined') {
-        // No value so add error to array (the Error summary)
-        errors.push({
-          text: 'You must select any options that apply',
-          href: '#applyToYou'
-        })
+    //   // Check if user has filled out a value
+    //   if (typeof req.session.data['penaltyReference'] === 'undefined') {
+    //     // No value so add error to array (the Error summary)
+    //     errors.push({
+    //       text: 'Enter a penalty reference number',
+    //       href: '#penaltyReference'
+    //     })
     
-        // Re-show page with error value as true so errors will show
-        res.render('v1/filter-question', {
-          errorApplyToYou: true,
-          errorList: errors
-        })
-      } else {
-            // User inputted a specific value, in this case 'none'
-            if (req.session.data['applyToYou'] == 'none') {   // this was === which doesn't work, changing to == seems to work?!
-              res.redirect('/v1/sign-in')
-            } else {
-              // User inputted any other value
-              res.redirect('/v1/stop-cannot-use-online-service')
-            }
-      }
-    })
+    //     // Re-show page with error value as true so errors will show
+    //     res.render('v1/penalty-details', {
+    //       errorPenaltyReference: true,
+    //       errorList: errors
+    //     })           
+    //   } else {
+    //       // If selected goes to next page
+    //       res.redirect('/v1/penalty-summary')
+    //     }           
+    //   })
   
   
 
