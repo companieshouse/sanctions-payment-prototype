8a20ecc9498ed40941687be6f152873eb7549315
mtuffs <mtuffs@companieshouse.gov.uk>
2024-02-19 11:49:17 +0000

    error messaging

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -5,6 +5,9 @@ module.exports = {
     "email": [ "user@testcompany.co.uk" ],
     //remember to put the comma back at the end of line 25!
     "contact-full-name": [ "Joe Bloggs" ],
+    "penalty-reference-number": [ "Y8294053" ],
+    "company-number-value": [ "28502425" ],
+    "company-number-value-wrong": [ "28502424" ],
     // Set the "are we signed in?" variable (gSignedIn) to FALSE as its default
     // "gSignedIn": [ "false" ],
     // signOutFirstTrigger is specific to the sign-out page to ensure the error messaging can come into play
