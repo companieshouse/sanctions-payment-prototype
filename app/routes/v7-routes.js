2340692787dd0a8dcace114b1e75372b1f6e1c84
Phoebe Radford (Companies House) <pradford@companieshouse.gov.uk>
2024-04-24 09:57:54 +0100

    New v7
    created new v7 views folder, and new v7 routes and replaced v6 with v7 in routes file

---
+++ app/routes/v7-routes.js
@@ -0,0 +1,83 @@
+//
+// For guidance on how to create routes see:
+// https://prototype-kit.service.gov.uk/docs/create-routes
+//
+
+const govukPrototypeKit = require('govuk-prototype-kit')
+const router = govukPrototypeKit.requests.setupRouter()
+
+// Add your routes here
+
+
+  // ******* filter-question ********************************
+  router.get('/v7/filter-question', function (req, res) {
+    // Set URl
+    res.render('v7/filter-question', {
+      currentUrl: req.originalUrl
+    })
+  })
+  
+
+
+  // ******* ERROR filter-question ********************************
+  router.post('/v7/filter-question', function (req, res) {
+    // Create empty array
+    var errors = []
+  
+    // Check if user has filled out a value
+    if (typeof req.session.data['applyToYou'] === 'undefined') {
+      // No value so add error to array (the Error summary)
+      errors.push({
+        text: 'Select which of these apply to the penalty, or select \'None of these\'',
+        href: '#applyToYou'
+      })
+  
+      // Re-show page with error value as true so errors will show
+      res.render('v7/filter-question', {
+        errorApplyToYou: true,
+        errorList: errors
+      })
+    } else {
+          // User inputted a specific value, in this case 'none'
+          if (req.session.data['applyToYou'] == 'none') {   // this was === which doesn't work, changing to == seems to work?!
+            res.redirect('/v7/sign-in')
+          } else {
+            // User inputted any other value
+            res.redirect('/v7/stop-you-need-to-pay-another-way')
+          }
+    }
+  })
+
+
+
+    // // ******* ERROR penalty details ********************************
+    // router.post('/v7/penalty-details', function (req, res) {
+    //   // Create empty array
+    //   var errors = []
+    
+    //   // Check if user has filled out a value
+    //   if (typeof req.session.data['penaltyReference'] === 'undefined') {
+    //     // No value so add error to array (the Error summary)
+    //     errors.push({
+    //       text: 'Enter a penalty reference number',
+    //       href: '#penaltyReference'
+    //     })
+    
+    //     // Re-show page with error value as true so errors will show
+    //     res.render('v7/penalty-details', {
+    //       errorPenaltyReference: true,
+    //       errorList: errors
+    //     })           
+    //   } else {
+    //       // If selected goes to next page
+    //       res.redirect('/v7/penalty-summary')
+    //     }           
+    //   })
+  
+  
+
+
+
+
+  module.exports=router;
+
